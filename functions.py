0e57ca4ccdb355f41679733d729f505ee026b7c8
Víctor Viloria <vvictor.97@gmail.com>
2024-04-04 18:14:20 +0200

    Parameters of selenium modified

--- functions.py
+++ functions.py
@@ -14,7 +14,9 @@ def obtener_avatar_kaggle(user):
     service = Service(ChromeDriverManager().install())
     # Dont open the browser
     options = webdriver.ChromeOptions()
-    options.add_argument("--headless") 
+    options.add_argument("--headless")
+    options.addArguments("--no-sandbox")
+    options.addArguments("--disable-dev-shm-usage")
     # Driver
     driver = webdriver.Chrome(service=service,options=options)
 
@@ -62,6 +64,8 @@ def create_kaggle_profile_card(user):
     service = Service(ChromeDriverManager().install())
     options = webdriver.ChromeOptions()
     options.add_argument("--headless")
+    options.addArguments("--no-sandbox")
+    options.addArguments("--disable-dev-shm-usage")
     driver = webdriver.Chrome(service=service, options=options)
 
     # Abre la URL objetivo
