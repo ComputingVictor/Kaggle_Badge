f384a031ce7352ccfc02e83609a7698c1bce9d0a
Víctor Viloria <vvictor.97@gmail.com>
2024-04-04 14:32:48 +0200

    Conda issues for the worflow fixed

--- script.py
+++ script.py
@@ -1,19 +1,18 @@
-# Importamos las funciones específicas desde el archivo functions.py
+import sys
 from functions import obtener_avatar_kaggle, create_kaggle_profile_card
 
-def main():
-    # Pedimos al usuario que introduzca el nombre de usuario de Kaggle
-    user = input("Por favor, introduce el nombre de usuario de Kaggle: ")
-
-
+def main(user):
     # Obtenemos el avatar del usuario de Kaggle
     avatar = obtener_avatar_kaggle(user)
     print(f"Avatar obtenido: {avatar}")
-
 
     # Creamos la tarjeta de perfil de Kaggle para el usuario
     profile_card = create_kaggle_profile_card(user)
     print("Tarjeta de perfil de Kaggle creada con éxito.")
 
 if __name__ == "__main__":
-    main()
+    if len(sys.argv) < 2:
+        print("Este script requiere un nombre de usuario de Kaggle como argumento.")
+        sys.exit(1)
+    user = sys.argv[1]
+    main(user)
